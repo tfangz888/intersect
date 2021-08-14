528539be3a1ba94053029a4a9390a704a7f0b32b
Tfangz <tfangz@ymail.com>
2021-08-14 23:36:00 +0800

    Add files via upload

---
+++ merge.cpp
@@ -0,0 +1,129 @@
+// g++ -o merge -std=c++2a merge.cpp -lstdc++
+// union 合并多个文件的所有行，且去重复
+// 文件可以有重复行，会改变原有顺序
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <filesystem>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+std::string& trim(std::string &s) 
+{
+    if (s.empty()) 
+    {
+        return s;
+    }
+
+    s.erase(0,s.find_first_not_of(" "));
+    s.erase(s.find_last_not_of(" ") + 1);
+
+    return s;
+}
+
+/*
+ * It will iterate through all the lines in file and
+ * put them in given vector
+ */
+bool getFileContent(std::string fileName, std::vector<std::string> & vecOfStrs)
+{
+    // Open the File
+    std::ifstream in(fileName.c_str());
+
+    // Check if object is valid
+    if(!in)
+    {
+        std::cerr << "Cannot open the File : "<<fileName<<std::endl;
+
+        return false;
+    }
+
+    std::string str;
+    // Read the next line from File untill it reaches the end.
+    while (std::getline(in, str))
+    {
+        // Line contains string of length > 0 then save it in vector
+		str = trim(str);
+        if(str.size() > 0)
+            vecOfStrs.push_back(str);
+    }
+    //Close The File
+    in.close();
+
+    return true;
+}
+
+//set、multiset都是集合类，差别在与set中不允许有重复元素，multiset中允许有重复元素
+//注意前后不要有空格
+std::vector<string> merge (const std::vector<std::vector<string>> &vecs) 
+{
+    auto last_intersection = vecs[0];
+    std::vector<string> curr_intersection;
+
+    for (std::size_t i = 1; i < vecs.size(); ++i) 
+    {
+	    vector<string> tmpVec = vecs[i];
+		std::sort(last_intersection.begin(), last_intersection.end());
+		std::sort(tmpVec.begin(), tmpVec.end());
+        std::merge(last_intersection.begin(), last_intersection.end(),
+            tmpVec.begin(), tmpVec.end(),
+            std::back_inserter(curr_intersection));
+        std::swap(last_intersection, curr_intersection);
+        curr_intersection.clear();
+    }
+
+    return last_intersection;
+}
+
+
+int main(int argc,char *argv[ ]) 
+{
+    vector<string> files;
+    // 检查文件存在
+    for (int i=1; i<argc; i++)
+	{
+	    string file = argv[i];
+		fs::path f{ file };
+		if (!fs::exists(f))
+		{
+		    cout << file << " not exist" << endl;
+
+		    return -1;
+		}
+		files.push_back(file);
+	}
+
+	std::vector<std::vector<string>> vecs;
+	for (string & file : files)
+	{
+	    std::vector<std::string> vecOfStr;
+	    // Get the contents of file in a vector
+        bool result = getFileContent(file, vecOfStr);
+		if(result)
+		{
+		    vecs.push_back(vecOfStr);
+		}
+		else
+		{
+		    cout << "read error " << file << endl;
+		}
+	}
+	vector<string> mergeSect = merge(vecs);
+    std::sort (mergeSect.begin(), mergeSect.end());
+    std::vector<string>::iterator it = std::unique (mergeSect.begin(), mergeSect.end());
+    mergeSect.resize( std::distance(mergeSect.begin(),it) );
+    for (it=mergeSect.begin(); it!=mergeSect.end(); ++it)
+        std::cout<<*it<<std::endl;
+
+    return 0;
+}
+
+
+
+
+
